685d8b1bfda10bbfed3d5e22f155861522f02a7a
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-10-16 18:26:13 +0300

    8ds

---
+++ datatypes/inde.js
@@ -0,0 +1,11 @@
+/**
+ * There are 8 data types in js
+ * number ie integer and floating point
+ * bigint eg 233n integer numbers of arbitrary length
+ * string
+ * boolean ie true or false
+ * null for unknown values
+ * undefined for unassigned values
+ * symbol for unique identifiers
+ * object for more complex data structures
+ */
